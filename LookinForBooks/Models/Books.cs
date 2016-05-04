c758b4a9c1bb46aa8513552ab596681b09115e9d
Brian Stickney <brianchase055@gmail.com>
2016-05-04 11:40:21 -0500

    Books and User Class

---
+++ LookinForBooks/Models/Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LookinForBooks.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int NumberofPages { get; set; }
+        public int ISBN { get; set; }
+}
---
+++ LookinForBooks/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Web;
+
+namespace LookinForBooks.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        
+    }
+}
