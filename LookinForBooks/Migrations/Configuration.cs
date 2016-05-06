f751ad31b93c706331b5f9250eed31a50bf6d1c3
Brian Stickney <brianchase055@gmail.com>
2016-05-06 14:01:14 -0500

    Enable migrations

---
+++ LookinForBooks/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace LookinForBooks.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LookinForBooks.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "LookinForBooks.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(LookinForBooks.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
