fa25a5a2224a7fa5437e13c735b37e3998fef4db
Brian Stickney <brianchase055@gmail.com>
2016-05-15 21:36:28 -0500

    working on dashboard displaying newley created books

--- LookinForBooks/Controllers/BooksController.cs
+++ LookinForBooks/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using LookinForBooks.Models;
@@ -18,8 +19,14 @@ namespace LookinForBooks.Controllers
             CurrentUser = db.Users.Find(userid);
             base.OnActionExecuting(filterContext);
         }
+
         private User CurrentUser;
         ApplicationDbContext db = ApplicationDbContext.Create();
+
+
+         
+      
+
 
         [HttpGet]
         public ActionResult DashBoard()
@@ -27,8 +34,8 @@ namespace LookinForBooks.Controllers
             var model = new DashBoardVm();
             model.MyBooks = CurrentUser.BooksIOwn.Select(b => new BookVm()
             {
-                IsCheckedOut = db.BookLoans.Any(x=>x.Book.Id == b.Id && x.CheckedIn == null),
-                BookId = b.Id, 
+                IsCheckedOut = db.BookLoans.Any(x => x.Book.Id == b.Id && x.CheckedIn == null),
+                BookId = b.Id,
                 Title = b.Title,
             }).ToList();
 
@@ -41,12 +48,77 @@ namespace LookinForBooks.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.AllUsers = new SelectList(db.Users.Select(u => new {u.Id, u.UserName}).ToList(), "Id", "UserName");
+
+            return View();
+        }
+
+        // POST: BooksTemp/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Author,Publisher,NumberofPages,Isbn")] Book book,
+            string selectedOwnerId)
+        {
+            if (ModelState.IsValid)
+            {
+                var owner = db.Users.Find(selectedOwnerId);
+
+                book.Owner = owner;
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("DashBoard");
+            }
+
+            return View(book);
+        }
+
+
         public ActionResult Details(int id)
         {
             //load the book from the database and populate a BookDetailsVM? and pass it to the view
             var modelBd = new BookDetailsVm();
             db.Books.Load();
             return View(modelBd);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            // throw new NotImplementedException();????
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = db.Books.Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Dashboard");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- LookinForBooks/Models/BookVMs.cs
+++ LookinForBooks/Models/BookVMs.cs
@@ -16,7 +16,11 @@ namespace LookinForBooks.Models
         public List<BookVm> MyBooks { get; set; } = new List<BookVm>();
         public List<BookVm> LoandedOutBooks { get; set; } = new List<BookVm>();
         public List<BookVm> AvailableBook { get; set; } = new List<BookVm>();
-         
+        public int NumberofPages { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Isbn { get; set; }
     }
 
     public class BookDetailsVm
